c5b7ae9d83d2ffe01f28fcb75368c2ac26c73bd1
uta630 <usuitkc.630@gmail.com>
2020-10-12 22:06:40 +0900

    add:メディアを取得してブラウザにカメラを表示

--- public/script.js
+++ public/script.js
@@ -1,6 +1,32 @@
 const socket = io('/'); // socket.ioがサーバーでセットしたlocalhost3030のルートパスとコネクトする
 const myPeer = new Peer(); // WebRTCを利用したP2P通信が簡単に実装できるライブラリ（http://yankee.cv.ics.saitama-u.ac.jp/~kunolab/yoshimi/peerjs-master/docs/）
 
+// videoタグ生成
+const myVideo = document.createElement('video');
+myVideo.muted = true; // 自分の声がデバイスから反射するのを防ぐ
+const videoWrap = document.getElementById('video-wrap'); // videoの出力先DOMのID
+
+// <video>に受け取ったストリームを追加する関数
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  // メタデータ読み込まれたときの処理, loadedmetadata = イベント名
+  video.addEventListener('loadedmetadata', () => {
+    video.play(); // videoの再生を実行
+  });
+
+  videoWrap.append(video); // videoの出力
+};
+
+// getUserMedia API : ユーザーの使用しているデバイスのメディアを取得
+// optionでvideo/audioの取得を指定することで、ビデオデータとオーディオを取得
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then((stream) => { // promise関数なのでthenで返り値を取得, 引数に動画情報が入る
+  // メディアを取得できたらvideoを出力
+  addVideoStream(myVideo, stream);
+})
+
 // Peerオブジェクトは生成時、ランダムなIDが付与されます, ここではuserId
 myPeer.on('open', userId => {
   // サーバーへのイベントの送信
